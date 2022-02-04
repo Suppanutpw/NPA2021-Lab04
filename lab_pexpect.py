c5e053e5c1e0efb660ca6d4eadb3be73470daeaf
62070186@kmitl.ac.th <changeme@example.com>
2022-02-04 10:27:14 +0000

    commit pexpect lab code

--- lab_pexpect.py
+++ lab_pexpect.py
@@ -3,19 +3,41 @@
 import pexpect
 
 PROMPT = '#'
-IP = '10.0.15.107'
+BASHIONIP = '10.0.15.107'
+ADDRESSES = [
+    ['172.31.107.4', '1.1.1.1'], 
+    ['172.31.107.5', '2.2.2.2'],
+    ['172.31.107.6', '3.3.3.3']
+]
 USERNAME = 'admin'
 PASSWORD = 'cisco'
 COMMAND = 'sh ip int bri'
 
-child = pexpect.spawn('telnet ' + IP)
+child = pexpect.spawn('telnet ' + BASHIONIP)
 child.expect('Username')
 child.sendline(USERNAME)
 child.expect('Password')
 child.sendline(PASSWORD)
 child.expect(PROMPT)
-child.sendline(COMMAND)
-child.expect(PROMPT)
-result = child.before
-print(result.decode('UTF-8'))
-child.sendline('exit')
+
+for address, loopback in ADDRESSES:
+    child.sendline('telnet ' + address)
+    child.expect('Username')
+    child.sendline(USERNAME)
+    child.expect('Password')
+    child.sendline(PASSWORD)
+    child.expect(PROMPT)
+    child.sendline('conf t')
+    child.expect(PROMPT)
+    child.sendline('int loopback 0')
+    child.expect(PROMPT)
+    child.sendline('ip address ' + loopback + ' 255.255.255.255')
+    child.expect(PROMPT)
+    child.sendline('exit') # interface config
+    child.expect(PROMPT)
+    child.sendline('exit') # config
+    child.expect(PROMPT)
+    child.sendline('write')
+    child.expect(PROMPT)
+    child.sendline('exit') # privilege
+    child.expect(PROMPT)
