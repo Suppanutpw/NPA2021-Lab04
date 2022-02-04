3da5c283238cc7b16c45be6e21243066af6847f1
62070186@kmitl.ac.th <changeme@example.com>
2022-02-04 16:32:14 +0000

    refactor code complete

---
+++ Paramiko/Homework2.py
@@ -0,0 +1,38 @@
+import time
+from jinja2 import Template
+import json
+import paramiko
+
+jsonFile = open("Routers.json", "r")
+routersJson = json.load(jsonFile)
+jsonFile.close()
+
+templateFile = open("template.j2", "r").read()
+template = Template(templateFile)
+templateFile.close()
+
+for routerJson in routersJson:
+    print(routerJson['Interfaces'])
+    commands = template.render(
+        interfaces = routerJson['Interfaces'],
+        ospf = routerJson['OSPF'],
+        vtyacl = routerJson['VTYACL'],
+        pat = routerJson.get('PAT', None)
+    )
+
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect (hostname=routerJson['IP'], username=routerJson['Username'], password=routerJson['Password'], look_for_keys=False)
+    print("Connecting to {}...".format(routerJson['IP']))
+
+    commands = commands.splitlines()
+    with client.invoke_shell() as ssh:
+        print("Connected to {}".format(routerJson['IP']))
+        for command in commands:
+            command = command.strip()
+            if (command):
+                ssh.send(command + "\n")
+                time.sleep(0.1)
+                result = ssh.recv(5000).decode('ascii')
+                print(result, end="")
+
