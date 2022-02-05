fc87d87f3f2f74757b40c68b66a687cb2320afbd
Arpo <arpo555@hotmail.com>
2022-02-05 17:40:07 +0700

    finished by convert private_key.ppk to openSSH, and update code

--- Paramiko/paramikolab02.py
+++ Paramiko/paramikolab02.py
@@ -2,18 +2,12 @@ import time
 import paramiko
 
 username = 'admin'
-# password = 'cisco'
-
-# devices_ip = ["172.31.107.4", "172.31.107.5", "172.31.107.6"]
-devices_ip = ["172.31.107.4"]
+devices_ip = ["172.31.107.4", "172.31.107.5", "172.31.107.6"]
 
 for ip in devices_ip:
     client = paramiko.SSHClient()
-    client.load_system_host_keys("paramiko_privateKey.ppk")
-    privateKey = client.get_host_keys()
-
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=ip, username=username, pkey=privateKey, look_for_keys=True)
+    client.connect(hostname=ip, username=username, key_filename="paramiko_privateKey", look_for_keys=True)
     print("Connecting to {} ...".format(ip))
     with client.invoke_shell() as ssh:
         print("Connected to {} ...".format(ip))
